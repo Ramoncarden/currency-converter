e9d208f48f53e834412cfd34ab2d2755601249fa
Ramon Cardenas <ramoncarden@gmail.com>
2021-10-09 00:50:04 -0500

    Added useEffect hook to CurrencyConverter component. Also made ui changes. Removed submit button.

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { data } from './data/data';
+import axios from 'axios';
+import NumberFormat from 'react-number-format';
 
-// const API_KEY = '763227896404298e9c6bc780';
-// const BASE_URL = 'https://v6.exchangerate-api.com/v6/63227896404298e9c6bc780/latest/USD';
+// TODO make API key into ENVIRONMENTAL VARIABLE
+const API_KEY = '763227896404298e9c6bc780';
+const BASE_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/pair`;
+//v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP
 
 const CurrencyConverter = () => {
   const [ formData, setFormData ] = useState({
@@ -9,34 +14,86 @@ const CurrencyConverter = () => {
     convertTo: 'GBP',
     amount: ''
   });
+  const [ conversionRate, setConversionRate ] = useState(null);
+  const [ error, setError ] = useState([]);
 
   function handleChange(evt) {
     const { name, value } = evt.target;
-    setFormData((data) => ({ ...data, [name]: value }));
+    setFormData((item) => ({ ...item, [name]: value }));
   }
 
-  async function handleSubmit(evt) {
-    evt.preventDefault();
-    console.log(formData);
-  }
+  /*   async function handleSubmit(evt) {
+    if (formData.amount !== '' || '') {
+      try {
+        const res = await axios.get(`${BASE_URL}/${formData.convertFrom}/${formData.convertTo}/${formData.amount}`);
+        let twoDecimalNum = Math.round(res.data.conversion_result * 100) / 100;
+        setConversionRate(twoDecimalNum);
+      } catch (e) {
+        setError(e.message);
+      }
+    } else {
+      setFormData[formData.amount] = 0;
+    }
+  } */
+
+  // const numForAmt = (evt) => {
+  //   // const re = /[+-]?\d+(?:[.,]\d+)?/;
+  //   const re = ^/(?!0\.00)[1-9]\d{0,2}(,\d{3})*(\.\d\d)?$/;
+  //   if (!re.test(evt.key)) {
+  //     evt.preventDefault();
+  //   }
+  // };
+
+  // Render component and make api call everytime state in formData changes.
+  useEffect(
+    () => {
+      async function handleSubmit(evt) {
+        if (formData.amount !== '') {
+          try {
+            const res = await axios.get(`${BASE_URL}/${formData.convertFrom}/${formData.convertTo}/${formData.amount}`);
+            let twoDecimalNum = Math.round(res.data.conversion_result * 100) / 100;
+            setConversionRate(twoDecimalNum);
+          } catch (e) {
+            setError(e.message);
+          }
+        } else {
+          setFormData[formData.amount] = '';
+        }
+      }
+      handleSubmit();
+    },
+    [ formData ]
+  );
 
   return (
     <div>
       <h1>Currency Converter</h1>
       <div className="convert-container">
-        <form onSubmit={handleSubmit}>
+        <form>
           <div className="convert-select">
             <div className="left-select">
-              <label className="select-input">Convert From: </label>
-              <select value={formData.convertFrom} onChange={handleChange}>
-                <option value="USD">USD</option>
+              <label htmlFor="convertFrom" className="select-input">
+                Convert From:{' '}
+              </label>
+              <select value={formData.convertFrom} name="convertFrom" onChange={handleChange}>
+                {data.map((item) => (
+                  <option value={item.code} key={item.name}>
+                    {item.code}
+                  </option>
+                ))}
               </select>
             </div>
 
             <div className="right-select">
-              <label className="select-input">Convert To: </label>
-              <select value={formData.convertTo} onChange={handleChange}>
-                <option value="GBP">GBP</option>
+              <label htmlFor="converTo" className="select-input">
+                Convert To:{' '}
+              </label>
+              <select value={formData.convertTo} name="convertTo" onChange={handleChange}>
+                {data.map((item) => (
+                  <option value={item.code} key={item.name}>
+                    {item.code}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
@@ -45,22 +102,36 @@ const CurrencyConverter = () => {
             <label htmlFor="amount" className="amount">
               Amount:
             </label>
-            <input
+            {/*             <input
               name="amount"
               id="amount"
-              type="text"
+              type="number"
+              required
+              maxLength={9}
               placeholder="Please enter amount"
               value={formData.amount}
               onChange={handleChange}
+            /> */}
+            <NumberFormat
+              name="amount"
+              id="amount"
+              value={formData.amount}
+              onChange={handleChange}
+              placeholder="Please enter amount"
+              allowNegative={false}
+              displayType="input"
+              allowLeadingZeros={false}
+              maxLength={9}
+              isNumericString={true}
+              title="Please enter a valid number"
             />
           </div>
 
-          <button type="submit" className="btn-grad">
-            Convert
-          </button>
+          {error && <div>{error}</div>}
         </form>
         <div className="result-area">
-          <h2>The result is: </h2>
+          <h2>Total: </h2>
+          {isNaN(conversionRate) ? '' : <p className="amt-total">{conversionRate}</p>}
         </div>
       </div>
     </div>
